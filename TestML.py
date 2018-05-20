460a9c63888191b9294d3e8c46cdd03d849edd1b
venkateswarlu <venkatesheee128@gmail.com>
2018-05-20 23:02:49 +0530

    ML Testing

--- TestML.py
+++ TestML.py
@@ -21,3 +21,49 @@ groups = fetch_20newsgroups()
 print (groups.keys())
 print (groups['target_names'])
 print (groups.target)
+
+import numpy as np
+print (np.unique(groups.target))
+print (groups.data[0])
+print (groups.target[0])
+print ( groups.target_names[groups.target[0]])
+print (len(groups.data[0]))
+print (len(groups.data[1]))
+
+import seaborn as sns
+sns.distplot(groups.target)
+import matplotlib.pyplot as plt
+plt.show()
+
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.datasets import fetch_20newsgroups
+cv = CountVectorizer(stop_words="english", max_features=500)
+groups = fetch_20newsgroups()
+transformed = cv.fit_transform(groups.data)
+print(cv.get_feature_names())
+sns.distplot(np.log(transformed.toarray().sum(axis=0)))
+plt.xlabel('Log Count')
+plt.ylabel('Frequency')
+plt.title('Distribution Plot of 500 Word Counts')
+plt.show()
+
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.datasets import fetch_20newsgroups
+from nltk.corpus import names
+from nltk.stem import WordNetLemmatizer
+def letters_only(astr):
+    return astr.isalpha()
+cv = CountVectorizer(stop_words="english", max_features=500)
+groups = fetch_20newsgroups()
+cleaned = []
+all_names = set(names.words())
+lemmatizer = WordNetLemmatizer()
+for post in groups.data:
+    cleaned.append(' '.join([lemmatizer.lemmatize(word.lower() for word in post.split() if letters_only(word) and word not in all_names]))
+transformed = cv.fit_transform(cleaned)
+print(cv.get_feature_names())
+
